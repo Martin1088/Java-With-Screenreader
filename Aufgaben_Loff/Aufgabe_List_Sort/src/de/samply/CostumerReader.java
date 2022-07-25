10c4d13f0b3944cef71c12bd9127e3eff2664b59
Martin1088 <96107909+Martin1088@users.noreply.github.com>
2022-07-25 14:48:36 +0200

    writing in a txt File

--- Aufgaben_Loff/Aufgabe_List_Sort/src/de/samply/CostumerReader.java
+++ Aufgaben_Loff/Aufgabe_List_Sort/src/de/samply/CostumerReader.java
@@ -1,4 +1,7 @@
 package de.samply;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.sql.*;
 
 public class CostumerReader {
@@ -11,14 +14,23 @@ public class CostumerReader {
                 Connection con = DriverManager.getConnection(url, user, password);
                 Statement stm = con.createStatement();
                 ResultSet rs = stm.executeQuery("SELECT * FROM kreditor");
+                // schreibe Liste in eine Datei txt
+                FileWriter myObj = new FileWriter("kreditoren.txt");
+
                 while (rs.next()) {
-                    for (int i = 1; i < 7; i++) {
+                    for (int i = 1; i < 6; i++) {
                         System.out.print(rs.getString(i)+ " " );
+                        myObj.write(rs.getString(i) + " ");
                     }
                     System.out.println();
+                    myObj.write("\r\n");
                 }
+                myObj.close();
             } catch (SQLException e) {
                 System.out.println(e.getMessage());
+            } catch (IOException e) {
+                System.out.println("There is something wrong !");
+                e.printStackTrace();
             }
     }
 }
