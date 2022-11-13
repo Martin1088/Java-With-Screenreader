491763171ad51444d915cceb5f319196ed074054
martin.jurk <martin.jurk@dkfz-heidelberg.de>
2022-11-13 19:18:53 +0100

    ende Berufschulewoche

--- RU/Array/src/Note.java
+++ RU/Array/src/Note.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class Note {
+public class Note {
+  public static void main(String[] args) {
+    double[] notenstatistik = {1.2, 2.3, 4.1, 2, 3.3, 3.6, 5.6, 2.3, 3.1, 2.2, 1.8};
+    // Index 0 ist n1 Anzahl von note 1 also immer plus 1
+    int[] n_mal = {0, 0, 0, 0, 0, 0};
+    double beste = 6;
+    double sum =0;
+    double durchschnitt = 0;
+    for (int i = 0;i < notenstatistik.length; i++) {
+      if (notenstatistik[i] <= 1.5) {n_mal[0]++;}
+      else if (notenstatistik[i] <= 2.5) {n_mal[1]++;}
+      else if (notenstatistik[i] <= 3.5) {n_mal[2]++;}
+      else if (notenstatistik[i] <= 4.5) {n_mal[3]++;}
+      else if (notenstatistik[i] <= 5.5) {n_mal[4]++;}
+      else {n_mal[5]++;}
+      if (beste > notenstatistik[i]) {
+        beste = notenstatistik[i];
+      }
+      sum += notenstatistik[i];
+    }
+    // Notenstatistik
+    for (int x = 0; x < n_mal.length; x++) {
+      System.out.println("Die Note " + (x +1) + " : " + n_mal[x] + " (" + ((100 / notenstatistik.length) * n_mal[x]) + "%) ");
+    }
+    // durchnitt der Noten errechnen
+    durchschnitt = sum / notenstatistik.length;
+    System.out.println("Beste Note: " + beste);
+    System.out.println("Notendurchschnitt: " + (Math.round(durchschnitt)));
+  }
+
 }
--- RU/Array/src/temperatur.java
+++ RU/Array/src/temperatur.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class temperatur {
+public class temperatur {
+  public static void main(String[] args) {
+    // Temperatur werte
+    Integer[] temperaturwerte = {19, 20, 40, 15, 17, 12, 4};
+    int max = 0;
+    int sum = 0;
+
+    for (int i = 0; i < temperaturwerte.length; i++) {
+      sum += temperaturwerte[i];
+      // Max wert
+      if (temperaturwerte[i] > max) {
+        max = temperaturwerte[i];
+      }
+      // Balken für das Diagramm als *
+      System.out.print("Temperatur " + (i + 1) + ":\t");
+      for (int x = 0; x < temperaturwerte[i]; x++ ) {
+        System.out.print("*");
+      }
+      System.out.print("\t" + "(" + temperaturwerte[i] + ")");
+      System.out.print("\n");
+    }
+    // Durchschnitt und max
+    System.out.println("Der Maxwert ist: " + max);
+    System.out.println("Die Durchschnittstemperatur: " + (sum / temperaturwerte.length));
+  }
 }
