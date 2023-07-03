37cc9a5baecf8c5fd1a81eff432101692955525e
martin.jurk <martin.jurk@dkfz-heidelberg.de>
2023-07-03 12:30:09 +0200

    IPv6 regex

--- Hartlieb/Kl/src/Ipv6.java
+++ Hartlieb/Kl/src/Ipv6.java
@@ -19,20 +19,10 @@ public class Ipv6 {
       }
       //System.out.println(test02[i]);
       for (int j = 0; j < test02[i].length(); j++) {
-        //System.out.print(test02[i].charAt(j));
-        int numberTest = (test02[i].charAt(j));
-        System.out.println(numberTest);
-        // zwischen 97 und 102 ist a - e
-        // ( numberTest < 0 && numberTest > 9 && numberTest < 97 && numberTest > 102)
-        if (numberTest < 0)  {
-          return false;
-        } else if (numberTest > 102) {
-          return false;
-        } else if ( numberTest > 9) {
-          if (numberTest < 97 ) {
-            return false;
-          }
-
+        String test =test02[i].substring(j, j+1);
+        boolean t = test.matches("[a-f0-9]");
+        if (t == false) {
+          System.out.println(test02[i].charAt(j));
         }
 
       }
@@ -42,7 +32,7 @@ public class Ipv6 {
   }
 
   public static void main(String[] args) {
-    String test = "abcd:ef:020c:f1ff:fe8e:c1da";
+    String test = "fe80::020c:f1ff:fe8e:c1da";
   System.out.println(checkIPv6(test));
   }
 }
