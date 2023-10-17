f71f2a18184eefee25cd897442cb97a5bfed7792
Martin Jurk <martin.jurk@dkfz-heidelberg.de>
2023-10-17 14:59:59 +0000

    tst

--- Rule/S2019/Pruefung_SO19/Nachricht.java
+++ Rule/S2019/Pruefung_SO19/Nachricht.java
@@ -1,30 +1,29 @@
 package Pruefung_SO19;
 
+import Pruefung_SO19.Person;
+
 public abstract class Nachricht {
 
 	protected int likes;
-	protected Person absender; //Assoziation gem. Klassendiagramm
+	protected Person absender; // Assoziation gem. Klassendiagramm
 
 	public Nachricht() {
 		super();
 		likes = 0;
 		absender = null;
 	}
-	
+
 	public Nachricht(Person absender) {
 		super();
 		likes = 0;
-		this.absender = absender; 
+		this.absender = absender;
 	}
 
-	
-	
 	public void hinzufuegenLike() {
 		likes++;
 	}
 
-	//Abstrakte Methode,diese muss in den Unterklassen ausprogrammiert werden
+	// Abstrakte Methode,diese muss in den Unterklassen ausprogrammiert werden
 	public abstract String getNachricht();
-		
-	
+
 }
