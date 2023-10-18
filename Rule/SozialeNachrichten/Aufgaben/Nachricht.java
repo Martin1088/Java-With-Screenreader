103d4122abcc516ce6bdbc8979adce0e477b87c4
Martin Jurk <martin.jurk@dkfz-heidelberg.de>
2023-10-18 12:18:05 +0000

    Class Test

---
+++ Rule/SozialeNachrichten/Aufgaben/Nachricht.java
@@ -0,0 +1,11 @@
+package Aufgaben;
+
+public class Nachricht {
+  protected Integer likes;
+  Person absender;
+
+  public Nachricht() {
+    super();
+    this.likes = 0;
+  }
+}
---
+++ Rule/SozialeNachrichten/Aufgaben/Person.java
@@ -0,0 +1,26 @@
+package Aufgabe;
+
+public class Person {
+  private String nachname;
+  private String vorname;
+
+  public Person() {
+    super();
+    this.nachname = "-----";
+    this.vorname = "-----";
+  }
+
+  public Person(String nachname, String vorname) {
+    super();
+    this.nachname = nachname;
+    this.vorname = vorname;
+  }
+
+  public String getNachname() {
+    return this.nachname;
+  }
+
+  public String getVorname() {
+    return this.vorname;
+  }
+}
