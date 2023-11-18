d04f4490ce0c65182a360c723360c526359f9950
Martin Jurk <martin.jurk@dkfz-heidelberg.de>
2023-11-18 10:27:50 +0000

    lösoung aufgbe passwor

--- Rule/password/Anmeldung.java
+++ Rule/password/Anmeldung.java
@@ -1,16 +1,27 @@
+
 /**
  * Anmeldung
  */
 public class Anmeldung {
-  public String getPasswort(String satz) {
+  public static String getPasswort(String satz) {
     // split und for , firt Uppercase then lowerCase
+    String result = "";
     String[] temp = satz.split(" ");
-    for (type var : iterable) {
-
+    Integer num = 0;
+    for (String word : temp) {
+      if ((num % 2) == 0) {
+        result += Character.toUpperCase(word.charAt(0));
+      } else {
+        result += Character.toLowerCase(word.charAt(0));
+      }
+      num += 1;
     }
+    return result;
   }
 
   public static void main(String[] args) {
-
+    String test = "Heute regnet es Am laufenden Bach";
+    String temp = getPasswort(test);
+    System.out.println(temp);
   }
 }
